f11f0f30e067e6d4c73a80310ee70618c181a8f9
Alex Sulsky <alex.sulsky@gmail.com>
2017-09-21 17:58:04 -0500

    moved hero2.py to parent dir

--- characters/hero.py
+++ characters/hero.py
@@ -1,23 +1,25 @@
+#Hero class
 import random
 from sys import path
 path += 'Users/alsu/Documents/code/DC2017/py/hero-rpg'
-
 from base import Base
 
 class Hero(Base):
-    # def __init__(self):
-    # super().__init__(self)
-    # def __init__(self, **kwargs):
-        # @classmethod
+        def __init__(self):
+            super().__init__(self)
+
         def attack_multiplier(self):
             if random.randint(1,5) == 2:
                 return 2
             else:
-                return 1
-        
+                return 1 
         def attack(self, defender):
             return Base.attack(self) * self.attack_multiplier()
-
+                
+        atk =  int(self.power + self.skill ** self.luck)
+        dfnse = defender.defense() - atk
+        if dfnse < 0:
+            return defender.health - dfnse
         def __str__(self):
             return str([self.name,
             self.power,
@@ -27,7 +29,7 @@ class Hero(Base):
             self.skill,
             self.bounty,
             self.luck,
-            self.attack(),
+            # self.attack(),
             self.defense(),
             self.alive()
             ])
